8828f90f04c6f14245ee910232cd903ea4a16893
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-05-20 22:30:21 +0300

    Add support for decoding temperature and level report events

--- src/RFM69MessageDecoder.ts
+++ src/RFM69MessageDecoder.ts
@@ -4,6 +4,8 @@ import RFM69Message from './RFM69Message'
 
 
 const AUTOPILOT_COMMAND_TAG = 'a'
+const TEMPERATURE_EVENT_TAG = 't'
+const LEVEL_REPORT_EVENT_TAG = 'r'
 
 export default function decodeRFM69Message(rfm69Msg: RFM69Message): ISensorEvent {
   const data = Buffer.from(rfm69Msg.data, 'hex')
@@ -13,6 +15,10 @@ export default function decodeRFM69Message(rfm69Msg: RFM69Message): ISensorEvent
   switch (tag) {
     case AUTOPILOT_COMMAND_TAG:
       return parseAutopilotCommand(data, meta)
+    case TEMPERATURE_EVENT_TAG:
+      return parseTemperatureEvent(data, meta)
+    case LEVEL_REPORT_EVENT_TAG:
+      return parseLevelReportEvent(data, meta)
     default:
       throw new Error(`Unexpected sensor tag: ${tag}`)
   }
@@ -28,6 +34,28 @@ function parseAutopilotCommand(buf: Buffer, { tag, ts, rssi }: EventMeta): Event
   const previousSampleTimeMicros = buf.readUInt32LE(6)
 
   return { tag, instance, ts, buttonId, isLongPress, vcc, previousSampleTimeMicros, rssi }
+}
+
+function parseTemperatureEvent(buf: Buffer, { tag, ts, rssi }: EventMeta): Events.ITemperatureEvent {
+  assertLength(buf, 'temperature event', 12)
+
+  const instance = buf.readUInt8(1).toString()
+  const temperature = buf.readFloatLE(2)
+  const vcc = buf.readInt16LE(6)
+  const previousSampleTimeMicros = buf.readUInt32LE(8)
+
+  return { tag, instance, ts, temperature, vcc, previousSampleTimeMicros, rssi }
+}
+
+function parseLevelReportEvent(buf: Buffer, { tag, ts, rssi }: EventMeta): Events.ILevelReportEvent {
+  assertLength(buf, 'level report event', 9)
+
+  const instance = buf.readUInt8(1).toString()
+  const level = buf.readUInt8(2)
+  const vcc = buf.readInt16LE(3)
+  const previousSampleTimeMicros = buf.readUInt32LE(5)
+
+  return { tag, instance, ts, level, vcc, previousSampleTimeMicros, rssi }
 }
 
 
